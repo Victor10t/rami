444fc143dc94ccbab26212545cab32eb3f1157b3
Victor10t <v.thomas49@outlook.fr>
2024-04-25 14:32:09 +0200

    distribuer v2 ok

--- distribuerv2.cpp
+++ distribuerv2.cpp
@@ -1,15 +1,23 @@
-/*void distribue_v2(int nb_joueurs, pioche & p){
-    hand * ens_main = new hand[nb_joueurs];
+/*void affiche_main(int nb_joueurs, hand * ens_main){
     for(int i = 0; i < nb_joueurs; ++i){
-        int point_depart = 103 - 14*i;
         std::cout << "\n Main n° " << i+1 << "\n";
         for(int j = 0; j < 14; ++j){
-            int a = rand() % (point_depart - j);
-            ens_main[i].c[j] = p.c[a];
-            carte z = p.c[a];
-            p.c[a] = p.c[point_depart - j];
-            p.c[point_depart - j] = z;
             std::cout << ens_main[i].c[j].valeur << " de " << ens_main[i].c[j].couleur << " | ";
         }
     }
+}
+
+hand * distribue_v3(int nb_joueurs, pioche & p){
+    hand * ens_main = new hand[nb_joueurs];
+    for(int i = 0; i < nb_joueurs; ++i){
+        for(int j = 0; j < 14; ++j){
+            int a = rand() % 104;
+            while(p.c[a].verif_d != true){
+                a = rand() % 104;
+            }
+            ens_main[i].c[j] = p.c[a];
+            p.c[a].verif_d = false;
+        }
+    }
+    return ens_main;
 }*/
